fc35ec35a5df48380b65553e061abf4d627b233a
Patryk <ge38wiw@mytum.de>
2022-01-21 17:51:18 +0100

    Revert "Added tkinter error handling"
    This reverts commit 202e9c118e1b56cf25bd86ed69852d6b465bd5d4.

--- BLURR/BlockingOverlay/MainBlockerWindow.py
+++ BLURR/BlockingOverlay/MainBlockerWindow.py
@@ -44,19 +44,11 @@ class MainBlockerWindow():
         self.root.bind("<FocusOut>", self.getFocus)
         # render GUI
         self.render()
-        self.startWindow()
-
-    def startWindow(self):
         # get focus to stay on top
         self.root.after(10, self.getFocus, 0)
         self.root.after(10, self.scanForMonitors)
         self.root.after(10, self.updateGui)
-        # to make sure window stays open even if some tkinter internal error occurs
-        try:
-            self.root.mainloop()
-        except Exception as exc:
-            print("Error tkinter")
-            self.startWindow()
+        self.root.mainloop()
 
     # render the overlay components
     def render(self):
